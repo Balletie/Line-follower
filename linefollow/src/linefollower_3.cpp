d0c50f126f9c81f65c3018e2d0957fba557421fa
Balletie <skip_meesie@hotmail.com>
2015-01-11 18:24:24 +0100

    Line detection...

--- linefollow/src/linefollower_3.cpp
+++ linefollow/src/linefollower_3.cpp
@@ -1,11 +1,33 @@
+/**
+ * Group number: 3
+ * Student 1:
+ * Skip Lentz, 4334051
+ * Student 2:
+ * Thomas Baars, 4212134
+ */
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
 #include <cv_bridge/cv_bridge.h>
+#include <vector>
 
 void imageCallback(const sensor_msgs::ImageConstPtr& img) {
-  try{
-    cv::imshow("Hello_World", cv_bridge::toCvShare(img, "bgr8")->image);
+  try {
+    cv::Mat cv_img, edge_img, color_edge_img;
+    cv_img = cv_bridge::toCvShare(img, "bgr8")->image;
+
+    cv::Canny(cv_img, edge_img, 50, 200, 3);
+    cv::cvtColor(edge_img, color_edge_img, CV_GRAY2BGR);
+
+    std::vector<cv::Vec4i> lines;
+    cv::HoughLinesP(edge_img, lines, 1, CV_PI/180, 80, 30, 10);
+    for(size_t i = 0; i < lines.size(); i++) {
+        cv::line(color_edge_img, cv::Point(lines[i][0], lines[i][1]),
+                                 cv::Point(lines[i][2], lines[i][3]),
+                                 cv::Scalar(0,0,255), 3, 8 );
+    }
+    cv::imshow("Hello_World", color_edge_img);
     cv::waitKey(30);
   } catch (cv_bridge::Exception& e) {
     ROS_ERROR("Could not convert from '%s' to 'bgr8'.", img->encoding.c_str());
@@ -15,8 +37,12 @@ void imageCallback(const sensor_msgs::ImageConstPtr& img) {
 int main(int argc, char** argv) {
   ros::init(argc, argv, "image_test");
   ros::NodeHandle nh;
+
   image_transport::ImageTransport it(nh);
-  image_transport::Subscriber sub = it.subscribe("camera/image", 1, imageCallback);
+  image_transport::TransportHints hints("compressed");
+  image_transport::Subscriber sub = it.subscribe("camera/image", 1,
+                                                 imageCallback, hints);
+
   cv::namedWindow("Hello_World");
   cv::startWindowThread();
   ros::spin();
